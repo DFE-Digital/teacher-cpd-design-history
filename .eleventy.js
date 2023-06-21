06439fc4a080431d28d53f4fed0e9681e4ed0fa7
Frankie Roberto <frankie@frankieroberto.com>
2023-06-21 12:06:34 +0100

    Use govuk-eleventy-plugin
    Switch to using the GOVUK Eleventy Plugin (for easier upgrades).

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,22 @@
 module.exports = function (eleventyConfig) {
+
+  // Options to customise the appearance of your design history
+  // https://x-govuk.github.io/govuk-eleventy-plugin/options/
+  eleventyConfig.addPlugin(require('@x-govuk/govuk-eleventy-plugin'), {
+    stylesheets: [
+      '/styles/application.css'
+    ],
+    headingPermalinks: true,
+    header: {
+      organisationLogo: false,
+      productName: 'Design history',
+      search: {
+        indexPath: '/search.json',
+        sitemapPath: '/sitemap'
+      }
+    }
+  })
+
   // Browser Sync
   eleventyConfig.setBrowserSyncConfig({
     rewriteRules: [{
@@ -13,10 +31,6 @@ module.exports = function (eleventyConfig) {
   // Template libraries
   eleventyConfig.setLibrary('njk', require('./lib/libraries/nunjucks'))
   eleventyConfig.setLibrary('md', require('./lib/libraries/markdown'))
-
-  // Plugins
-  eleventyConfig.addPlugin(require('@11ty/eleventy-navigation'))
-  eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-syntaxhighlight'))
 
   // Filters
   eleventyConfig.addFilter('date', require('./lib/filters/date'))
