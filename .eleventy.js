1f070186aefe4ac258eb870c1ed48a082f426dc8
Jackie DAlcorn <53426640+dalcJ@users.noreply.github.com>
2026-06-05 14:05:42 +0100

    Adding a new collection for NPD and the skeleton for some posts.

--- .eleventy.js
+++ .eleventy.js
@@ -45,6 +45,10 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addCollection('ecf-v2', collection => {
     return collection.getFilteredByGlob('app/posts/ecf-v2/*.md')
   })
+
+  eleventyConfig.addCollection('npd-courses', collection => {
+    return collection.getFilteredByGlob('app/posts/npd-courses/*.md')
+  })
   // Browser Sync
   eleventyConfig.setBrowserSyncConfig({
     rewriteRules: [{
